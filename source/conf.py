29adc7c18711a6ddaf87225be2e44f83df46f684
fengwencheng66 <116816727+fengwencheng66@users.noreply.github.com>
2025-10-07 10:21:53 +0800

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -7,7 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 import sphinx_rtd_theme
-# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # import recommonmark
 
