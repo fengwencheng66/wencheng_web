dd73c4c25a3c029a5cb30bb92172ebd3c7220d45
fengwencheng66 <116816727+fengwencheng66@users.noreply.github.com>
2025-10-08 08:37:26 +0800

    update logo

--- source/conf.py
+++ source/conf.py
@@ -33,7 +33,7 @@ exclude_patterns = []
 # html_static_path = ['_static']
 
 html_theme ='sphinx_rtd_theme'
-html_static_path = ('_static')
+html_static_path = ['_static']
 html_logo = 'logo.png' 
 html_theme_options = {
     'logo_only': False,
