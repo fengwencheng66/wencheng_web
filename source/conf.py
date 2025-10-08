2b20ef2df23e982956ee782b2bab393e063f0739
fengwencheng66 <116816727+fengwencheng66@users.noreply.github.com>
2025-10-08 08:42:29 +0800

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -34,7 +34,7 @@ exclude_patterns = []
 
 html_theme ='sphinx_rtd_theme'
 html_static_path = ['_static']
-html_logo = 'logo.png' 
+html_logo = 'logo01.png' 
 html_theme_options = {
     'logo_only': False,
     'display_version': False,
